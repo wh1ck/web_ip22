96c3317e38839a67650455000d853a5d916c7acf
wh1ck <107947245+wh1ck@users.noreply.github.com>
2024-04-25 14:15:12 +0700

    Add files via upload

--- auth.php
+++ auth.php
@@ -1,3 +1,41 @@
+<?php
+require 'connect.php';
+require 'core.php';
+
+# ищем пользователя по логину и паролю
+$auth_username = isset($_POST['username']) ? $_POST['username'] : false;
+$auth_password = isset($_POST['password']) ? $_POST['password'] : false;
+$auth_type = isset($_POST['type']) ? $_POST['type'] : false; # тип post-запроса
+$array_messages = array();
+if($auth_type && $auth_type == 'auth'){
+    # действия, выполняемые при авторизации
+    if(!$auth_username){
+        # логин не введен
+        array_push($array_messages, 'Логин не введен');
+    }
+    if(!$auth_password){
+        # пароль не введен
+        array_push($array_messages, 'Пароль не введен');
+    }
+    # 'захэшируем пароль'
+    $auth_password = md5($auth_password);
+    if(!count($array_messages)){
+        # пока ошибок не найдено, ищем пользователя
+        $find_user = $mysqli->query("SELECT username, password FROM members WHERE username='$auth_username' AND password='$auth_password' LIMIT 1");
+        # проверяем, найден ли пользователь
+        if(!$find_user->num_rows){
+            # не найден
+            array_push($array_messages, 'Пользователь не найден');
+        } else {
+            # найден
+            $user_data = $find_user->fetch_assoc();
+            $_SESSION['username'] = $user_data['username'];
+            $_SESSION['password'] = $user_data['password'];
+            header('Location: /index.php');
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +45,7 @@
 </head>
 <body>
     <form action="/auth.php" method="post">
+        <input type="hidden" name="type" value="auth">
         <input type="text" name="username" placeholder="Логин" required>
         <br>
         <input type="password" name="password" placeholder="Пароль" required>
--- reg.php
+++ reg.php
@@ -38,39 +38,95 @@ if($reg_type == 'reg'){
         # фамилия не введена
         array_push($array_messages, 'Фамилия не введена');
     }
+    # поиск пользователя по логину
+    $find_username = $mysqli->query("SELECT id FROM members WHERE username='$reg_username' LIMIT 1");
+        if($find_username->num_rows){
+            array_push($array_messages, 'Пользователь уже существует');
+        }
     # проверки закончены.
     if(!count($array_messages)){
         # регистрацию продолжать можно
         $result = $mysqli->query("INSERT INTO members (username,password,name,surename) VALUES('$reg_username','$reg_password','$reg_your_name','$reg_surename')");
+        header('Location: /auth.php');
     }
 }
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Регистрация</title>
-</head>
-<body>
-    <?php
-    foreach($array_messages as $value){
-        echo($value . '<br>');
-    }
-    ?>
-    <form action="/reg.php" method="POST">
-        <input type="hidden" name="type" value="reg">
-        <input type="text" placeholder="Логин" name="username" required>
-        <br>
-        <input type="password" placeholder="Пароль" name="password" required>
-        <br>
-        <input type="password" placeholder="Повторите пароль" name="prepeat" required>
-        <br>
-        <input type="text" placeholder="Имя" name="your_name" required>
-        <br>
-        <input type="text" placeholder="Фамилия" name="surename" required>
-        <br>
-        <input type="submit" value="Отправить">
-    </form>
-</body>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="bg-body-tertiary">
+    <div class="container">
+        <main>
+            <div class="py-5 text-center">
+                <h2>Регистрация</h2>
+            </div>
+            <div class="row g-5">
+      <div class="col-md-5 col-lg-4 order-md-last">
+        <ul class="list-group mb-3">
+          <li class="list-group-item d-flex justify-content-between">
+            <span>Total (USD)</span>
+            <strong>$20</strong>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-7 col-lg-8">
+        <h4 class="mb-3">Заполните ваши данные</h4>
+        <form class="needs-validation" novalidate>
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="firstName" class="form-label">Имя</label>
+              <input type="text" class="form-control" id="firstName" placeholder="Имя" value="" name="your_name" required>
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="lastName" class="form-label">Фамилия</label>
+              <input type="text" class="form-control" id="lastName" placeholder="Фамилия" value="" required>
+              <div class="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+
+            <div class="col-12">
+              <label for="username" class="form-label">Логин</label>
+              <div class="input-group has-validation">
+                <span class="input-group-text">@</span>
+                <input type="text" class="form-control" id="username" placeholder="Логин" required>
+              <div class="invalid-feedback">
+                  Your username is required.
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <label for="firstName" class="form-label">Пароль</label>
+              <input type="password" class="form-control" id="firstName" placeholder="Пароль" value="" required>
+              <div class="invalid-feedback">
+                Valid first name is required.
+              </div>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="lastName" class="form-label">Подтвердите пароль</label>
+              <input type="password" class="form-control" id="lastName" placeholder="Подтвердите пароль" value="" required>
+              <div class="invalid-feedback">
+                Valid last name is required.
+              </div>
+            </div>
+          <hr class="my-4">
+
+          <button class="w-100 btn btn-primary btn-lg" type="submit">Создать аккаунт</button>
+        </form>
+      </div>
+    </div>
+        </main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </html>
